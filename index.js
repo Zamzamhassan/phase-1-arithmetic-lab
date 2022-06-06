f40f559111c00cfafe228e698d0f219adc72f15c
Zamzam <Zamzamhassan44@gmail.com>
2022-06-06 23:31:05 +0300

    js arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+ var num1=31, num2 =2;
+ var multiply =(31 * 2);
+ multiply;
+
+ 
+var random;
+random = Math.floor(Math.random() * 10) + 1;
+  
+var num3=24, num4=5;
+var mod =(24 % 5);
+mod;
+
+let max = Math.max(5 ,10, 15, 20);
